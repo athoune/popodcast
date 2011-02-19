30612d6540ce6ea308f0601299afbbaf5571ce46
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-19 20:37:58 +0100

    cute download

--- popodcast.py
+++ popodcast.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tiny podcast downloader
+"""
+
+__author__ = "Mathieu Lecarme <mathieu@garambrogne.net>"
 
 import ConfigParser
 import feedparser
@@ -8,8 +15,16 @@ import os
 import os.path
 import shutil
 
-def download(block, size, total_size):
-	print 100 * block * size / total_size
+class Download(object):
+	"Cute download display"
+	value = 0
+	def __init__(self, title):
+		self.title = title
+	def __call__(self, block, size, total_size):
+ 		v = 100 * block * size / total_size
+		if v != self.value:
+			self.value = v
+			print "\033[1A\033[K%s [%i %%]" % (self.title, v)
 
 conf = ConfigParser.ConfigParser()
 conf.read('./popodcast.ini')
@@ -19,12 +34,10 @@ for section in conf.sections():
 	if not os.path.exists(d.feed.title):
 		os.makedirs(d.feed.title)
 	for entry in d.entries:
-		print "\t", entry.title
-		if not os.path.exists(entry.title):
-			os.makedirs(entry.title)
+		if not os.path.exists(section):
+			os.makedirs(section)
 		for mp3 in [link for link in entry.links if link.type == u'audio/mpeg']:
-			print mp3
-			file = os.path.join(d.feed.title, "%s_%s.mp3" % (entry.title, time.strftime("%Y-%m-%d_%H-%M-%S", entry.updated_parsed)))
+			file = os.path.join(section, "%s_%s.mp3" % (time.strftime("%Y-%m-%d_%H-%M-%S", entry.updated_parsed), entry.title))
 			if not os.path.exists(file):
-				urllib.urlretrieve(mp3.href, 'tmp.mp3', download)
+				urllib.urlretrieve(mp3.href, 'tmp.mp3', Download("%s: %s" % (d.feed.title, entry.title)))
 				shutil.move('tmp.mp3', file)
