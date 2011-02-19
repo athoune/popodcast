aec055e8b23ac88aedb5f6cc1043bf52ebc6d0c8
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-19 14:21:33 +0100

    mp3

--- popodcast.py
+++ popodcast.py
@@ -7,4 +7,8 @@ conf = ConfigParser.ConfigParser()
 conf.read('./popodcast.ini')
 for section in conf.sections():
 	d = feedparser.parse(conf.get(section, 'rss'))
-	print d
+	print d.feed.title
+	for entry in d.entries:
+		print "\t", entry.title
+		for mp3 in [link for link in entry.links if link.type == u'audio/mpeg']:
+			print mp3
